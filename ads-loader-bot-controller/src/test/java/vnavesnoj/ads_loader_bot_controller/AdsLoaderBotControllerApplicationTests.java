a15ef13eccbefd95e0db8ef69af7b25594f40a45
vnavesnoj <vnavesnoj@gmail.com>
2024-06-12 20:11:09 +0300

    Add ads-loader-bot-controller module

---
+++ ads-loader-bot-controller/src/test/java/vnavesnoj/ads_loader_bot_controller/AdsLoaderBotControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package vnavesnoj.ads_loader_bot_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsLoaderBotControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
